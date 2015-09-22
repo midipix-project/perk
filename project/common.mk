305bc956d8e39d6bf9e7295d26a6fef7fdd6e800
midipix <writeonce@midipix.org>
2015-09-22 06:08:13 -0400

    build system: normalize (alpha-sort) the list of source C files.

--- project/common.mk
+++ project/common.mk
@@ -1,13 +1,13 @@
 COMMON_SRCS = \
 	src/main/pe_get_image_meta.c \
 	src/main/pe_map_raw_image.c \
-	src/reader/pe_read_import_header.c \
-	src/reader/pe_read_export_header.c \
-	src/reader/pe_read_dos_header.c \
+	src/output/pe_output_export_symbols.c \
 	src/reader/pe_read_coff_header.c \
-	src/reader/pe_read_section_header.c \
+	src/reader/pe_read_dos_header.c \
+	src/reader/pe_read_export_header.c \
+	src/reader/pe_read_import_header.c \
 	src/reader/pe_read_optional_header.c \
-	src/output/pe_output_export_symbols.c
+	src/reader/pe_read_section_header.c \
 
 APP_SRCS = \
 	src/main/perk.c
