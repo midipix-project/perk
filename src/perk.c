846f96ecfe93b8f3cb08b0baa8296f20b7a3974b
midipix <writeonce@midipix.org>
2015-11-22 23:07:55 -0500

    driver integration 9/9: utilize the driver in the front-end utility.

--- src/perk.c
+++ src/perk.c
@@ -1,11 +1,46 @@
+#include <stdio.h>
 #include <perk/perk.h>
 
-static int perk_main(int argc, char ** argv, char ** envp)
+#ifndef PERK_DRIVER_FLAGS
+#define PERK_DRIVER_FLAGS	PERK_DRIVER_VERBOSITY_ERRORS|PERK_DRIVER_VERBOSITY_USAGE
+#endif
+
+static void perk_perform_unit_actions(struct pe_unit_ctx * uctx)
 {
-	return -1;
+	uint64_t flags = uctx->cctx.fmtflags;
+
+	if (flags & PERK_OUTPUT_FLAG_EXPORT_SYMS)
+		uctx->cctx.status = pe_output_export_symbols(uctx->meta,&uctx->cctx,0);
 }
 
-int main(int argc, char ** argv, char ** envp)
+static int perk_exit(struct pe_driver_ctx * dctx, int status)
+{
+	pe_free_driver_ctx(dctx);
+	return (status < 0) ? 2 : status;
+}
+
+static int perk_main(int argc, const char ** argv, const char ** envp)
+{
+	int			ret;
+	struct pe_driver_ctx *	dctx;
+	struct pe_unit_ctx *	uctx;
+	const char **		unit;
+
+	if ((ret = pe_get_driver_ctx(argv,envp,PERK_DRIVER_FLAGS,&dctx)))
+		return (ret == PERK_USAGE) ? 0 : 2;
+
+	for (unit=dctx->units; *unit; unit++) {
+		if (!(pe_get_unit_ctx(dctx,*unit,&uctx))) {
+			perk_perform_unit_actions(uctx);
+			ret = uctx->cctx.status ? uctx->cctx.status : ret;
+			pe_free_unit_ctx(uctx);
+		}
+	}
+
+	return perk_exit(dctx,ret);
+}
+
+int main(int argc, const char ** argv, const char ** envp)
 {
 	return perk_main(argc,argv,envp);
 }
