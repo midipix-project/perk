c3d8abcbdcab5566b2c390193716f40009122d9a
midipix <writeonce@midipix.org>
2015-12-12 22:03:28 -0500

    whitespace meditation.

--- src/skin/pe_skin_default.c
+++ src/skin/pe_skin_default.c
@@ -2,27 +2,26 @@
 #include "argv/argv.h"
 
 const struct argv_option pe_default_options[] = {
-	{"version",	'v',TAG_VERSION,ARGV_OPTARG_NONE,	0,0,
-								"show version information"},
+	{"version",	'v',TAG_VERSION,ARGV_OPTARG_NONE,0,0,
+			"show version information"},
 
-	{"help",	'h',TAG_HELP,	ARGV_OPTARG_OPTIONAL,	"short|long",0,
-								"show usage information "
-								"[listing %s options only]"},
+	{"help",	'h',TAG_HELP,ARGV_OPTARG_OPTIONAL,"short|long",0,
+			"show usage information [listing %s options only]"},
 
-	{"output",	'o',TAG_OUTPUT,	ARGV_OPTARG_REQUIRED,	0,"<file>",
-								"write output to %s"},
+	{"output",	'o',TAG_OUTPUT,ARGV_OPTARG_REQUIRED,0,"<file>",
+			"write output to %s"},
 
-	{"pretty",	'p',TAG_PRETTY,	ARGV_OPTARG_REQUIRED,	"yaml",0,
-								"format output for parsing by %s"},
+	{"pretty",	'p',TAG_PRETTY,ARGV_OPTARG_REQUIRED,"yaml",0,
+			"format output for parsing by %s"},
 
-	{"expsyms",	'e',TAG_EXPSYMS,ARGV_OPTARG_NONE,	0,0,
-								"print exported symbols" },
+	{"expsyms",	'e',TAG_EXPSYMS,ARGV_OPTARG_NONE,0,0,
+			"print exported symbols" },
 
-	{"implibs",	'i',TAG_IMPLIBS,ARGV_OPTARG_NONE,	0,0,
-								"list direct dependency libraries"},
+	{"implibs",	'i',TAG_IMPLIBS,ARGV_OPTARG_NONE,0,0,
+			"list direct dependency libraries"},
 
-	{"impsyms",	'I',TAG_IMPSYMS,ARGV_OPTARG_NONE,	0,0,
-								"list direct dependency libraries "
-								"along with required symbols"},
+	{"impsyms",	'I',TAG_IMPSYMS,ARGV_OPTARG_NONE,0,0,
+			"list direct dependency libraries "
+			"along with required symbols"},
 	{0}
 };
