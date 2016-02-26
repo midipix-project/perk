e5d4bb6e04112cca5fe4fc79048ede646caced30
midipix <writeonce@midipix.org>
2016-02-26 17:09:04 -0500

    build system: --toolchain: initial implementation.

---
+++ sysinfo/toolchain/llvm.mk
@@ -0,0 +1,17 @@
+AS		= llvm-mc -filetype=obj
+AR		= llvm-ar
+LD		= lld
+NM		= llvm-nm
+OBJDUMP		= llvm-objdump
+RANLIB		= llvm-ranlib
+SIZE		= llvm-size
+STRIP		= $(CROSS_COMPILE)strip
+STRINGS		= $(CROSS_COMPILE)strings
+
+
+ADDR2LINE	= $(CROSS_COMPILE)addr2line
+COV		= $(CROSS_COMPILE)gcov
+CXXFILT		= $(CROSS_COMPILE)c++filt
+ELFEDIT		= $(CROSS_COMPILE)elfedit
+OBJCOPY		= $(CROSS_COMPILE)objcopy
+READELF		= $(CROSS_COMPILE)readelf
