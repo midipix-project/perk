53f310856b1ecd00f48aa4bd8d962ce9b13909e2
midipix <writeonce@midipix.org>
2016-05-05 15:29:42 -0400

    build system: Makefile.in, custom.mk: added --all-[static|shared] support.

--- project/custom.mk
+++ project/custom.mk
@@ -5,3 +5,27 @@ endif
 ifeq ($(OS_BINFMT),ELF)
 include $(PROJECT_DIR)/sysinfo/os/elf.mk
 endif
+
+
+
+ifeq ($(ALL_STATIC),yes)
+
+package-app:	static-app
+app:		PACKAGE_APP = $(STATIC_APP)
+app-tag:	PACKAGE_APP = $(STATIC_APP)
+
+
+else ifeq ($(ALL_SHARED),yes)
+
+package-app:	shared-app
+app:		PACKAGE_APP = $(SHARED_APP)
+app-tag:	PACKAGE_APP = $(SHARED_APP)
+
+
+else
+
+package-app:	default-app
+app:		PACKAGE_APP = $(DEFAULT_APP)
+app-tag:	PACKAGE_APP = $(DEFAULT_APP)
+
+endif
