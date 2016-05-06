8d8242d854343bc5e252b6e91967a309792122df
midipix <writeonce@midipix.org>
2016-05-06 10:06:28 -0400

    pe_read_section_header(): integrate runtime little endian test.

--- src/reader/pe_read_section_header.c
+++ src/reader/pe_read_section_header.c
@@ -4,10 +4,10 @@
 /*  Released under GPLv2 and GPLv3; see COPYING.PERK.          */
 /***************************************************************/
 
-#include <endian.h>
 #include <string.h>
 
 #include <perk/perk.h>
+#include "perk_endian_impl.h"
 #include "perk_reader_impl.h"
 
 int pe_read_section_header(const struct pe_sec_hdr * p, struct pe_meta_sec_hdr * m)
@@ -19,25 +19,25 @@ int pe_read_section_header(const struct pe_sec_hdr * p, struct pe_meta_sec_hdr *
 	/* todo: long name support */
 	m->long_name = 0;
 
-	#if (BYTE_ORDER == LITTLE_ENDIAN)
+	if (PERK_LITTLE_ENDIAN) {
 
-	memcpy(&m->virtual_size,&p->virtual_size,sizeof(*p)-sizeof(p->name));
+		memcpy(&m->virtual_size,&p->virtual_size,sizeof(*p)-sizeof(p->name));
 
-	#else
+	} else {
 
-	m->virtual_size		= pe_read_long(p->virtual_size);
-	m->virtual_addr		= pe_read_long(p->virtual_addr);
-	m->size_of_raw_data	= pe_read_long(p->size_of_raw_data);
-	m->ptr_to_raw_data	= pe_read_long(p->ptr_to_raw_data);
-	m->ptr_to_relocs	= pe_read_long(p->ptr_to_relocs);
-	m->ptr_to_line_nums	= pe_read_long(p->ptr_to_line_nums);
+		m->virtual_size		= pe_read_long(p->virtual_size);
+		m->virtual_addr		= pe_read_long(p->virtual_addr);
+		m->size_of_raw_data	= pe_read_long(p->size_of_raw_data);
+		m->ptr_to_raw_data	= pe_read_long(p->ptr_to_raw_data);
+		m->ptr_to_relocs	= pe_read_long(p->ptr_to_relocs);
+		m->ptr_to_line_nums	= pe_read_long(p->ptr_to_line_nums);
 
-	m->num_of_relocs	= pe_read_short(p->num_of_relocs);
-	m->num_of_line_nums	= pe_read_short(p->num_of_line_nums);
+		m->num_of_relocs	= pe_read_short(p->num_of_relocs);
+		m->num_of_line_nums	= pe_read_short(p->num_of_line_nums);
 
-	m->characteristics	= pe_read_long(p->characteristics);
+		m->characteristics	= pe_read_long(p->characteristics);
 
-	#endif
+	}
 
 	return 0;
 }
