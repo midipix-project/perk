e539245a66095b67ad86c5452f44b976f8dfd257
midipix <writeonce@midipix.org>
2016-05-10 21:05:15 -0400

    build system: PE targets: added import library --avoid-version support.

--- sysinfo/os/pe.mk
+++ sysinfo/os/pe.mk
@@ -5,7 +5,7 @@ LDFLAGS_SHARED	+= $(LDFLAGS_IMPLIB)
 LDFLAGS_SHARED	+= -Wl,-soname
 LDFLAGS_SHARED	+= -Wl,$(DSO_SONAME)
 
-implib:			implib-ver implib-soname implib-solink
+implib:			implib-ver package-implib-soname package-implib-solink
 
 implib-ver:		shared-lib $(IMPLIB_VER)
 
@@ -18,6 +18,37 @@ $(IMPLIB_DEF):		shared-lib
 $(IMPLIB_VER):		$(IMPLIB_DEF)
 			$(DLLTOOL) -l $(IMPLIB_VER) -d $(IMPLIB_DEF) -D $(DSO_VER)
 
+install-implib:		install-implib-ver \
+			package-install-implib-soname \
+			package-install-implib-solink
+
+install-implib-ver:	implib-ver
+			mkdir -p $(DESTDIR)$(LIBDIR)
+			cp $(IMPLIB_VER) $(DESTDIR)$(LIBDIR)
+
+clean-implib:
+			rm -f $(SHARED_LIB)
+			rm -f $(IMPLIB_DEF)
+			rm -f $(IMPLIB_VER)
+			rm -f $(IMPLIB_SONAME)
+			rm -f $(IMPLIB_SOLINK)
+
+
+ifeq ($(AVOID_VERSION),yes)
+
+package-implib-soname:
+package-implib-solink:
+package-install-implib-soname:
+package-install-implib-solink:
+
+else
+
+package-implib-soname:		implib-soname
+package-implib-solink:		implib-solink
+package-install-implib-soname:	install-implib-soname
+package-install-implib-solink:	install-implib-solink
+
+
 $(IMPLIB_SONAME):	$(IMPLIB_DEF)
 			$(DLLTOOL) -l $(IMPLIB_SONAME) -d $(IMPLIB_DEF) -D $(DSO_SONAME)
 
@@ -25,14 +56,6 @@ $(IMPLIB_SOLINK):	$(IMPLIB_SONAME)
 			rm -f $(IMPLIB_SOLINK).tmp
 			ln -s $(IMP_SONAME) $(IMPLIB_SOLINK).tmp
 			mv $(IMPLIB_SOLINK).tmp $(IMPLIB_SOLINK)
-
-install-implib:		install-implib-ver \
-			install-implib-soname \
-			install-implib-solink
-
-install-implib-ver:	implib-ver
-			mkdir -p $(DESTDIR)$(LIBDIR)
-			cp $(IMPLIB_VER) $(DESTDIR)$(LIBDIR)
 
 install-implib-soname:	implib-soname
 			mkdir -p $(DESTDIR)$(LIBDIR)
@@ -44,9 +67,4 @@ install-implib-solink:	implib-soname
 			ln -s $(IMP_SONAME) $@.tmp
 			mv $@.tmp $(DESTDIR)$(LIBDIR)/$(IMP_SOLINK)
 
-clean-implib:
-			rm -f $(SHARED_LIB)
-			rm -f $(IMPLIB_DEF)
-			rm -f $(IMPLIB_VER)
-			rm -f $(IMPLIB_SONAME)
-			rm -f $(IMPLIB_SOLINK)
+endif
