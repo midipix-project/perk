wiedi/slibtool

project: added tag version information.

project/tagver.mk
@@ -1,0 +1,5 @@
+VER_NAMESPACE	= SLBT
+
+VER_MAJOR	= 0
+VER_MINOR	= 0
+VER_PATCH	= 0

midipix-project/perk

project/tagver.mk
@@ -1,0 +1,5 @@
+VER_NAMESPACE	= PERK
+
+VER_MAJOR	= 0
+VER_MINOR	= 0
+VER_PATCH	= 0