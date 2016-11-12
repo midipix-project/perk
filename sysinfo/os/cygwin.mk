47bbd3a595a392accae7c0d27305e6510ca11354
midipix <writeonce@midipix.org>
2016-11-11 22:24:33 -0500

    build system: sysinfo: expand the OS gallery.

---
+++ sysinfo/os/cygwin.mk
@@ -0,0 +1,12 @@
+OS		= cygwin
+OS_APP_PREFIX	=
+OS_APP_SUFFIX	= .exe
+OS_LIB_PREFIX	= lib
+OS_LIB_SUFFIX	= .dll
+OS_IMPLIB_EXT	= .dll.a
+OS_LIBDEF_EXT	= .def
+OS_ARCHIVE_EXT	= .a
+OS_SONAME	= copy
+OS_BINFMT	= PE
+
+CFLAGS_PIC	=
