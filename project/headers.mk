10fddce6a8cbdbb5181442b538d2ae8fcbf8376b
midipix <writeonce@midipix.org>
2016-11-14 14:52:02 -0500

    project: headers.mk: code maintenance.

--- project/headers.mk
+++ project/headers.mk
@@ -1,16 +1,16 @@
 API_HEADERS = \
-	$(PROJECT_DIR)/include/$(PACKAGE)/perk.h \
-	$(PROJECT_DIR)/include/$(PACKAGE)/perk_api.h \
-	$(PROJECT_DIR)/include/$(PACKAGE)/perk_consts.h \
-	$(PROJECT_DIR)/include/$(PACKAGE)/perk_meta.h \
-	$(PROJECT_DIR)/include/$(PACKAGE)/perk_output.h \
-	$(PROJECT_DIR)/include/$(PACKAGE)/perk_structs.h
+	$(SOURCE_DIR)/include/$(PACKAGE)/perk.h \
+	$(SOURCE_DIR)/include/$(PACKAGE)/perk_api.h \
+	$(SOURCE_DIR)/include/$(PACKAGE)/perk_consts.h \
+	$(SOURCE_DIR)/include/$(PACKAGE)/perk_meta.h \
+	$(SOURCE_DIR)/include/$(PACKAGE)/perk_output.h \
+	$(SOURCE_DIR)/include/$(PACKAGE)/perk_structs.h
 
 INTERNAL_HEADERS = \
-	$(PROJECT_DIR)/src/internal/argv/argv.h \
-	$(PROJECT_DIR)/src/internal/$(PACKAGE)_driver_impl.h \
-	$(PROJECT_DIR)/src/internal/$(PACKAGE)_endian_impl.h \
-	$(PROJECT_DIR)/src/internal/$(PACKAGE)_errinfo_impl.h \
-	$(PROJECT_DIR)/src/internal/$(PACKAGE)_reader_impl.h \
+	$(SOURCE_DIR)/src/internal/argv/argv.h \
+	$(SOURCE_DIR)/src/internal/$(PACKAGE)_driver_impl.h \
+	$(SOURCE_DIR)/src/internal/$(PACKAGE)_endian_impl.h \
+	$(SOURCE_DIR)/src/internal/$(PACKAGE)_errinfo_impl.h \
+	$(SOURCE_DIR)/src/internal/$(PACKAGE)_reader_impl.h \
 
 ALL_HEADERS = $(API_HEADERS) $(INTERNAL_HEADERS)
