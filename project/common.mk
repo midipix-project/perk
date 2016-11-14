2b1427447cf44b07b153425645b719e02380c16e
midipix <writeonce@midipix.org>
2016-11-14 14:51:01 -0500

    project: common.mk: code maintenance.

--- project/common.mk
+++ project/common.mk
@@ -1,8 +1,7 @@
-COMMON_SRCS = \
+API_SRCS = \
 	src/driver/pe_amain.c \
 	src/driver/pe_driver_ctx.c \
 	src/driver/pe_unit_ctx.c \
-	src/internal/perk_errinfo_impl.c \
 	src/logic/pe_get_image_meta.c \
 	src/logic/pe_map_raw_image.c \
 	src/output/pe_output_error.c \
@@ -17,5 +16,10 @@ COMMON_SRCS = \
 	src/reader/pe_read_section_header.c \
 	src/skin/pe_skin_default.c \
 
+INTERNAL_SRCS = \
+	src/internal/$(PACKAGE)_errinfo_impl.c \
+
 APP_SRCS = \
 	src/perk.c
+
+COMMON_SRCS = $(API_SRCS) $(INTERNAL_SRCS)
