4d1375c67e0e2b91395777d811473beff7f409b2
midipix <writeonce@midipix.org>
2016-11-16 18:48:57 -0500

    reader: pe_read_section_header(): remove useless optimization.

--- src/reader/pe_read_section_header.c
+++ src/reader/pe_read_section_header.c
@@ -16,27 +16,18 @@ int pe_read_section_header(const struct pe_sec_hdr * p, struct pe_meta_sec_hdr *
 	memset(m,0,sizeof(m->name));
 	memcpy(m,p,sizeof(p->name));
 
-	m->long_name = 0;
+	m->long_name		= 0;
+	m->virtual_size		= pe_read_long(p->virtual_size);
+	m->virtual_addr		= pe_read_long(p->virtual_addr);
+	m->size_of_raw_data	= pe_read_long(p->size_of_raw_data);
+	m->ptr_to_raw_data	= pe_read_long(p->ptr_to_raw_data);
+	m->ptr_to_relocs	= pe_read_long(p->ptr_to_relocs);
+	m->ptr_to_line_nums	= pe_read_long(p->ptr_to_line_nums);
 
-	if (PERK_LITTLE_ENDIAN) {
+	m->num_of_relocs	= pe_read_short(p->num_of_relocs);
+	m->num_of_line_nums	= pe_read_short(p->num_of_line_nums);
 
-		memcpy(&m->virtual_size,&p->virtual_size,sizeof(*p)-sizeof(p->name));
-
-	} else {
-
-		m->virtual_size		= pe_read_long(p->virtual_size);
-		m->virtual_addr		= pe_read_long(p->virtual_addr);
-		m->size_of_raw_data	= pe_read_long(p->size_of_raw_data);
-		m->ptr_to_raw_data	= pe_read_long(p->ptr_to_raw_data);
-		m->ptr_to_relocs	= pe_read_long(p->ptr_to_relocs);
-		m->ptr_to_line_nums	= pe_read_long(p->ptr_to_line_nums);
-
-		m->num_of_relocs	= pe_read_short(p->num_of_relocs);
-		m->num_of_line_nums	= pe_read_short(p->num_of_line_nums);
-
-		m->characteristics	= pe_read_long(p->characteristics);
-
-	}
+	m->characteristics	= pe_read_long(p->characteristics);
 
 	return 0;
 }
