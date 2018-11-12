153cea12f066ec60d625449765e28fc112085546
midipix <writeonce@midipix.org>
2018-11-12 00:26:35 -0500

    output: header dump: added pe_hdrdump_coff_image_hdr().

--- project/common.mk
+++ project/common.mk
@@ -3,6 +3,7 @@ API_SRCS = \
 	src/driver/pe_driver_ctx.c \
 	src/driver/pe_unit_ctx.c \
 	src/hdrdump/pe_hdrdump_image_dos_hdr.c \
+	src/hdrdump/pe_hdrdump_coff_image_hdr.c \
 	src/info/pe_get_image_abi.c \
 	src/info/pe_get_image_framework.c \
 	src/info/pe_get_image_subsystem.c \
